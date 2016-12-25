98b455ab59a5e1ec8620fe3c6bdf3bd806fdbd74
stary <yzx9610@outlook.com>
2016-12-25 11:45:53 +0800

    change pages

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -15,7 +15,7 @@ app.controller('homeArticlesListCtrl', function ($scope, $http) {
     };
 
     function searchByTitle(title) {
-        for (var each of $scope.articlesList) {
+        for (var each of $scope.articlesGlobalList) {
             if (each['title'] == title) {
                 return each['id'];
             }
@@ -27,14 +27,57 @@ app.controller('homeArticlesListCtrl', function ($scope, $http) {
             url: '../loadArticles',
             method: 'GET'
         }).success(function (data) {
-            $scope.articlesList = data['articlesList'];
-            console.log($('#content').html());
-            $('#content').html(data['articlesList'][0]['content']);
+            $scope.articlesGlobalList = data['articlesList'];
             autocompleteSearch(data['articlesList']);
+            showPerPage();
         }).error(function (error) {
             console.log(error);
         })
     }
+
+    function showPerPage() {
+        $scope.totalPages = Math.ceil($scope.articlesGlobalList.length / 5);
+        $scope.nowPages = 1;
+        $scope.page = [];
+        $scope.articlesList = [];
+        computeNav();
+        computeArticles();
+    }
+
+    function computeNav() {
+        var isActive = '';
+        for (var index = 1; index <= $scope.totalPages; index++) {
+            if (index == $scope.nowPages) {
+                isActive = 'active';
+            } else {
+                isActive = '';
+            }
+            $scope.page.push({
+                index: index,
+                class: isActive
+            })
+        }
+    }
+
+    function computeArticles() {
+        $scope.articlesList = [];
+        $scope.articlesList = $scope.articlesGlobalList.slice(($scope.nowPages - 1) * 5, $scope.nowPages * 5);
+        console.log($scope.articlesList);
+    }
+
+    $scope.turnTo = function (index) {
+        NavchangeToPage($scope.nowPages, index);
+        $scope.nowPages = index;
+        computeArticles();
+        console.log($scope.articlesList);
+    };
+
+    function NavchangeToPage(org, des) {
+        $scope.page[org - 1]['class'] = '';
+        $scope.page[des - 1]['class'] = 'active';
+    }
+
+
 
     function autocompleteSearch(articlesList) {
         var availableTags = [];
